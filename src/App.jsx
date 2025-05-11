87146bf9cb7f1ba624bca6da31941a75003dc11a
Lindsey <lawsonlindsey36@yahoo.com>
2025-05-11 12:10:08 -0400

    update bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './index.css';
 import Layout from './components/Layout.jsx';
 import Home from './pages/Home.jsx';
 import Pokedex from './pages/Pokedex.jsx';
 
-
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="pokedex" element={<Pokedex />} />
-      </Route>
-    </Routes>
+    <Router basename="/Pokemon-explorer">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="pokedex" element={<Pokedex />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
